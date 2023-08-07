e2060f5a9a984259dbf1cefca57702d7ee0a8cbb
HapaxHypatia <r.haskmann@gmail.com>
2023-08-07 20:35:25 +1000

    standardising entry point. GUI window

--- transcribe.py
+++ transcribe.py
@@ -3,6 +3,17 @@ import os
 import tempfile
 import shutil
 import time
+import tkinter as tk
+from tkinter import simpledialog
+
+def getFile():
+	ROOT = tk.Tk()
+	ROOT.withdraw()
+	# # the input dialog
+	USER_INP = simpledialog.askstring(title="filepath",
+                                  prompt="Path to your audio file:")
+	return USER_INP
+
 
 filetypes = (
 	"cda",
@@ -10,39 +21,50 @@ filetypes = (
 	"wav",
 	"wma",
 )
+
 start = time.time()
 print ("Loading model, please wait...")
 model = whisper.load_model("medium")
 print("Model loaded in " + str(round(time.time()-start)) +' seconds.')
 
 
-def transcribeFile(filename, outpath):
+def transcribeFile(filename, destDIR):
 	print("Transcribing " + filename)
 	start = time.time()
 	transcription = model.transcribe(filename, language="fr", fp16=False)
 	textlength = len(transcription["text"].split(' '))
 	if textlength>5:	#Dump any transcriptions of only a few words
+		file_stats = os.stat(filename)
+		log = open('logs.txt', 'a', encoding='utf-8')
+		log.write(filename)
+		log.write(str(file_stats.st_size))
 		print("Writing transcription of " +filename )
+		#Create transcriptions folder in specified directory
+		outpath = destDIR + '//Transcriptions//' + filename[:-4].replace('\\', '-') + ".txt"
 		with open(outpath, 'a', encoding='utf-8') as outfile:
 			outfile.write(filename+'\n')
 			outfile.write(transcription['text'])
-			print("File transcribed in " + str(round(time.time()-start)) +' seconds')
+			log.write("File transcribed in " + str(round(time.time()-start)) +' seconds')
 
-DIR = input('Paste directory path here: ')
-os.mkdir(DIR + '//Transcriptions')
-
-# Walk through all files in DIR including in subdirectories
-# Find audio files and transcribe them, save resulting text file to Transcriptions folder
-for subdir, dirs, files in os.walk(DIR):
-	if subdir == "Transcriptions":
-		continue
-	for f in files:
-		if f.lower().endswith(filetypes):
-			outpath = DIR + '//Transcriptions//' + f[:-4] + ".txt"
-			if not os.path.isfile(outpath):
-				transcribeFile(os.path.join(DIR, subdir, f), outpath)
+def transcribeDIR(DIR):
+	# Walk through all files in DIR including in subdirectories
+	# Find audio files and transcribe them, save resulting text file to Transcriptions folder
+	for subdir, dirs, files in os.walk(DIR):
+		if subdir == "Transcriptions":
+			continue
+		for f in files:
+			outpath = destDIR + '//Transcriptions//' + f[:-4] + ".txt"
+			if f.lower().endswith(filetypes):
+				if not os.path.isfile(outpath):
+					transcribeFile(os.path.join(DIR, subdir, f), DIR)
 
 
-
-
-
+if __name__ == '__main__':
+	filepath = getFile()
+	destDIR = os.path.dirname(filepath)
+	if not os.path.isdir(destDIR + '//Transcriptions'):
+		os.mkdir(destDIR + '//Transcriptions')
+	if os.path.isdir(filepath):
+		transcribeDIR(filepath)
+	else:
+		transcribeFile(filepath, destDIR)
